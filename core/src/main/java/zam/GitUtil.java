8716a66319bc0600afb56814f71136987bb4f7f9
Peter Donald <peter@realityforge.org>
2018-02-25 10:56:33 +1100

    Import some logic from zim

--- core/src/main/java/zam/GitUtil.java
+++ core/src/main/java/zam/GitUtil.java
@@ -2,8 +2,11 @@ package zam;
 
 import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 public final class GitUtil
 {
@@ -63,5 +66,119 @@ public final class GitUtil
     throws Exception
   {
     ExecUtil.capture( b -> b.command( "git", "clean", "-f", "-d", "-x" ) );
+  }
+
+  public static void pull()
+    throws Exception
+  {
+    pull( null );
+  }
+
+  public static void pull( @Nullable final String remote )
+    throws Exception
+  {
+    ExecUtil.system( b -> ExecUtil.cmd( b, "git", "pull", remote ) );
+  }
+
+  public static void push()
+    throws Exception
+  {
+    push( null );
+  }
+
+  public static void push( @Nullable final String remote )
+    throws Exception
+  {
+    final String currentBranch = currentBranch();
+    ExecUtil.system( b -> ExecUtil.cmd( b, "git", "push", "--set-upstream", remote, currentBranch ) );
+  }
+
+  /**
+   * Return the current branch.
+   *
+   * @return the current branch.
+   */
+  @Nonnull
+  public static String currentBranch()
+    throws Exception
+  {
+    return ExecUtil.capture( b -> b.command( "git", "rev-parse", "--abbrev-ref", "HEAD" ) ).replace( "\n", "" );
+  }
+
+  /**
+   * Return the local branches.
+   *
+   * @return the local branches.
+   */
+  public static List<String> localBranches()
+    throws Exception
+  {
+    return branches( false );
+  }
+
+  /**
+   * Return the branches.
+   *
+   * @param all true if all branches should be returned.
+   * @return the branches.
+   */
+  public static List<String> branches( final boolean all )
+    throws Exception
+  {
+    return Arrays
+      .stream( ExecUtil
+                 .capture( b -> b.command( "git", "branch", all ? "--all" : null ) )
+                 .split( "\n" ) )
+      .filter( line -> !line.contains( " -> " ) )
+      .map( line -> line.substring( 2 ) )
+      .collect( Collectors.toList() );
+  }
+
+  public static void resetBranch()
+    throws Exception
+  {
+    resetBranch( null );
+  }
+
+  /**
+   * Reset the index.
+   */
+  public static void resetIndex()
+    throws Exception
+  {
+    ExecUtil.capture( b -> ExecUtil.cmd( b, "git", "reset" ) );
+  }
+
+  /**
+   * Reset the index, local filesystem and potentially branch.
+   */
+  public static void resetBranch( @Nullable final String branch )
+    throws Exception
+  {
+    ExecUtil.capture( b -> ExecUtil.cmd( b, "git", "reset", "--hard", branch ) );
+    clean();
+  }
+
+  public static void checkout()
+    throws Exception
+  {
+    checkout( "master" );
+  }
+
+  public static void checkout( @Nonnull final String branch )
+    throws Exception
+  {
+    checkout( branch, false );
+  }
+
+  public static void checkout( @Nonnull final String branch, final boolean createUnlessPresent )
+    throws Exception
+  {
+    final boolean create =
+      createUnlessPresent &&
+      branches( true )
+        .stream()
+        .anyMatch( b -> b.equals( branch ) && b.equals( "remotes/origin/" + branch ) );
+    ExecUtil.capture( b -> ExecUtil.cmd( b, "git", "checkout", create ? "-b" : null, branch ) );
   }
 }
