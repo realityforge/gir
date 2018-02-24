d77b3666f78555d4a4c697b75b08e36ebb1ef5f5
Peter Donald <peter@realityforge.org>
2018-02-25 10:59:57 +1100

    Move example code to separate package

--- core/src/main/java/zam/Zam.java
+++ core/src/main/java/zam/Zam.java
@@ -1,8 +1,5 @@
 package zam;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 public final class Zam
 {
   private static ZamContext c_context = new ZamContext();
@@ -10,22 +7,5 @@ public final class Zam
   public static ZamContext context()
   {
     return c_context;
-  }
-
-  public static void main( String[] args )
-    throws Exception
-  {
-    final Path workingDirectory = Paths.get( "/Users/peter/Code/zam" );
-    IoUtil.inDirectory( workingDirectory, () -> {
-      Git.clone( "https://github.com/realityforge/antix.git", "antix" );
-      IoUtil.inDirectory( workingDirectory.resolve( "antix" ), () -> {
-        Git.fetch();
-        Git.resetBranch();
-        Git.checkout();
-        Git.pull();
-      } );
-    } );
-
-    Zam.context().close();
   }
 }
---
+++ core/src/main/java/zam/example/Example1.java
@@ -0,0 +1,27 @@
+package zam.example;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import zam.Git;
+import zam.IoUtil;
+import zam.Zam;
+
+public final class Example1
+{
+  public static void main( String[] args )
+    throws Exception
+  {
+    final Path workingDirectory = Paths.get( "/Users/peter/Code/zam" );
+    IoUtil.inDirectory( workingDirectory, () -> {
+      Git.clone( "https://github.com/realityforge/antix.git", "antix" );
+      IoUtil.inDirectory( workingDirectory.resolve( "antix" ), () -> {
+        Git.fetch();
+        Git.resetBranch();
+        Git.checkout();
+        Git.pull();
+      } );
+    } );
+
+    Zam.context().close();
+  }
+}
