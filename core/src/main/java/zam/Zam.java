d317cf4a058fed75de40d1164f5f1c8521bb54e8
Peter Donald <peter@realityforge.org>
2018-02-25 07:06:51 +1100

    Experiments in the code

--- core/src/main/java/zam/Zam.java
+++ core/src/main/java/zam/Zam.java
@@ -1,5 +1,29 @@
 package zam;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public final class Zam
 {
+  private static ZamContext c_context = new ZamContext();
+
+  public static ZamContext context()
+  {
+    return c_context;
+  }
+
+  public static void main( String[] args )
+    throws Exception
+  {
+    final Path workingDirectory = Paths.get( "/Users/peter/Code/zam" );
+    IoUtil.inDirectory( workingDirectory, () -> {
+      GitUtil.clone( "https://github.com/realityforge/antix.git", "antix" );
+      IoUtil.inDirectory( workingDirectory.resolve( "antix" ), () -> {
+        GitUtil.fetch();
+        GitUtil.clean();
+      } );
+    } );
+
+    Zam.context().close();
+  }
 }
