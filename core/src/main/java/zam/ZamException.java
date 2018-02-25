a792c9f500d369cc63e77bd5856ce2f4933550a3
Peter Donald <peter@realityforge.org>
2018-02-25 18:42:19 +1100

    Add a Zam exception so easy to throw runtime exceptions that we know we expect

---
+++ core/src/main/java/zam/ZamException.java
@@ -0,0 +1,24 @@
+package zam;
+
+public class ZamException
+  extends RuntimeException
+{
+  public ZamException()
+  {
+  }
+
+  public ZamException( final String message )
+  {
+    super( message );
+  }
+
+  public ZamException( final String message, final Throwable cause )
+  {
+    super( message, cause );
+  }
+
+  public ZamException( final Throwable cause )
+  {
+    super( cause );
+  }
+}
