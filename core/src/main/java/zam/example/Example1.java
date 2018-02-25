c7581f6287c32949edab3fa8261733e710e7a04b
Peter Donald <peter@realityforge.org>
2018-02-25 15:09:22 +1100

    Update example

--- core/src/main/java/zam/example/Example1.java
+++ core/src/main/java/zam/example/Example1.java
@@ -3,6 +3,7 @@ package zam.example;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import zam.Zam;
+import zam.delta.Patch;
 import zam.git.Git;
 import zam.io.IoUtil;
 
@@ -14,11 +15,18 @@ public final class Example1
     final Path workingDirectory = Paths.get( "/Users/peter/Code/zam" );
     IoUtil.inDirectory( workingDirectory, () -> {
       Git.clone( "https://github.com/realityforge/antix.git", "antix" );
-      IoUtil.inDirectory( workingDirectory.resolve( "antix" ), () -> {
+      final Path appDirectory = workingDirectory.resolve( "antix" );
+      IoUtil.inDirectory( appDirectory, () -> {
         Git.fetch();
         Git.resetBranch();
         Git.checkout();
         Git.pull();
+        final Path file = appDirectory.resolve( "buildfile" );
+        if ( Patch.file( file, c -> c.replaceAll( "1\\.6", "1.8" ) ) )
+        {
+          Git.add( file.toString() );
+          Git.commit( "Patched version of java from 1.6 to 1.8" );
+        }
       } );
     } );
 
