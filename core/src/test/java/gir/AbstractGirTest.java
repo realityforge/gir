dd86d16896dc2857d86d8b675d3df9bde8cff039
Peter Donald <peter@realityforge.org>
2018-03-03 14:33:43 +1100

    Remofve unused util methods

--- core/src/test/java/gir/AbstractGirTest.java
+++ core/src/test/java/gir/AbstractGirTest.java
@@ -3,11 +3,7 @@ package gir;
 import gir.io.FileUtil;
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Field;
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 import org.realityforge.braincheck.BrainCheckTestUtil;
-import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import static org.testng.Assert.*;
@@ -27,36 +23,6 @@ public abstract class AbstractGirTest
     throws Exception
   {
     BrainCheckTestUtil.resetConfig( true );
-  }
-
-  @Nonnull
-  private Field getField( @Nonnull final Class<?> type, @Nonnull final String fieldName )
-    throws NoSuchFieldException
-  {
-    Class clazz = type;
-    while ( null != clazz && Object.class != clazz )
-    {
-      try
-      {
-        final Field field = clazz.getDeclaredField( fieldName );
-        field.setAccessible( true );
-        return field;
-      }
-      catch ( final Throwable t )
-      {
-        clazz = clazz.getSuperclass();
-      }
-    }
-
-    Assert.fail();
-    throw new IllegalStateException();
-  }
-
-  @SuppressWarnings( "SameParameterValue" )
-  final void setField( @Nonnull final Object object, @Nonnull final String fieldName, @Nullable final Object value )
-    throws NoSuchFieldException, IllegalAccessException
-  {
-    getField( object.getClass(), fieldName ).set( object, value );
   }
 
   protected final File createTempDirectory()
