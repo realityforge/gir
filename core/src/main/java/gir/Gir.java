c538a095c2d1d73cda41e51ff704d1269c2ad022
Peter Donald <peter@realityforge.org>
2018-03-04 13:29:45 +1100

    Add some basic javadocs

--- core/src/main/java/gir/Gir.java
+++ core/src/main/java/gir/Gir.java
@@ -1,5 +1,8 @@
 package gir;
 
+/**
+ * The top level class from which to access Gir facilities.
+ */
 public final class Gir
 {
   private Gir()
@@ -8,6 +11,12 @@ public final class Gir
 
   private static GirContext c_context = new GirContext();
 
+  /**
+   * Return the current context.
+   * There can only be one activate context at any one time.
+   *
+   * @return the current context.
+   */
   public static GirContext context()
   {
     return c_context;
